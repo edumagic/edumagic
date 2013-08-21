f3934170c1222c2ce219d3227a0062535794e767
loginov.alex.valer <loginov.alex.valer@a3ee4a2f-5db2-278e-6c24-8350288542b0>
2013-08-21 11:25:32 +0000

    git-svn-id: http://magicos.googlecode.com/svn/trunk@72 a3ee4a2f-5db2-278e-6c24-8350288542b0

--- files/patches/rootfs/MagOS-mplayer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PFP=etc/mplayer/mplayer.conf
-sed -i s/'ao=pulse,alsa,oss,'/'ao=alsa,oss,pulse,'/ $PFP
-sed -i s/'.slang = en'/'slang = ru'/ $PFP
-sed -i s/'.alang = en'/'alang = ru'/ $PFP
-grep -q "qdbus org.freedesktop.ScreenSaver" $PFP || echo 'heartbeat-cmd="qdbus org.freedesktop.ScreenSaver /ScreenSaver SimulateUserActivity"' >> $PFP
-exit 0
--- files/patches/rootfs/MagOS-proxychains.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PFP=usr/bin/proxychains
-sed -i s%LD_PRELOAD=.*libproxychains%'LD_PRELOAD=/usr/lib/libproxychains'% $PFP
-exit 0
